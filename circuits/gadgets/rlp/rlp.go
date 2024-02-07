0d293497748f47b2ce4d9d3491cdb3d9f1f10f94
Jie <jie@celer.network>
2024-02-07 20:50:12 +0800

    test 8K tx calldata

--- circuits/gadgets/rlp/rlp.go
+++ circuits/gadgets/rlp/rlp.go
@@ -22,27 +22,27 @@ type Transaction struct {
 }
 
 var TxLeafArrayRlpParams = ArrayCheck{
-	MaxHexLen:            3264,
+	MaxHexLen:            16320,
 	MaxFields:            2,
 	ArrayPrefixMaxHexLen: 4,
 	FieldMinHexLen:       []int{0, 0},
-	FieldMaxHexLen:       []int{2, 3258},
+	FieldMaxHexLen:       []int{2, 16312},
 }
 
 var SignedTxArrayCheckParams = ArrayCheck{
-	MaxHexLen:            3258,
+	MaxHexLen:            16312,
 	MaxFields:            12,
 	ArrayPrefixMaxHexLen: 6,
 	FieldMinHexLen:       []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	FieldMaxHexLen:       []int{16, 16, 16, 16, 8, 40, 64, 2950, 2, 2, 64, 64},
+	FieldMaxHexLen:       []int{16, 16, 16, 16, 8, 40, 64, 16000, 2, 2, 64, 64},
 }
 
 var UnsignedTxArrayRlpParams = ArrayCheck{
-	MaxHexLen:            3124,
+	MaxHexLen:            15866,
 	MaxFields:            2,
 	ArrayPrefixMaxHexLen: 4,
 	FieldMinHexLen:       []int{0, 0},
-	FieldMaxHexLen:       []int{2, 3118},
+	FieldMaxHexLen:       []int{2, 15858},
 }
 
 // DecodeTxLeafRlp decode transaction raw rlp, decode depth fixed 2 nested rlp array of tx rlp
